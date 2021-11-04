8e49760ef5d1ffa1bec6b7243885923b2ea1ecda
Gabriel Tavares da Silva <93527457+gabrieltavares184@users.noreply.github.com>
2021-11-03 22:00:04 -0300

    Rename Exercício 27.cpp to exerc27.cpp

--- exerc27.cpp
+++ exerc27.cpp
@@ -1,11 +1,11 @@
-/* Exercício 27: Faça um programa no qual sejam declaradas duas variáveis do tipo int, uma variável do tipo float e sejam lidos valores para elas. 
-A seguir, mostre um resultado em cada linha para as operações:
+/* ExercÃ­cio 27: FaÃ§a um programa no qual sejam declaradas duas variÃ¡veis do tipo int, uma variÃ¡vel do tipo float e sejam lidos valores para elas. 
+A seguir, mostre um resultado em cada linha para as operaÃ§Ãµes:
 
-  • A soma dos dois valores inteiros;
-  • O dobro do primeiro valor inteiro somado com o
+  â€¢ A soma dos dois valores inteiros;
+  â€¢ O dobro do primeiro valor inteiro somado com o
     dobro do segundo valor inteiro;
-  • O valor float multiplicado por 6.5;
-  • A soma dos três valores. */
+  â€¢ O valor float multiplicado por 6.5;
+  â€¢ A soma dos trÃªs valores. */
 
 //------------------------------------------------Bibliotecas------------------------------------------------------------//
 
@@ -13,7 +13,7 @@ A seguir, mostre um resultado em cada linha para as operações:
 #include <stdlib.h>
 #include <locale.h>
 
-//------------------------------------------------Funções----------------------------------------------------------------//
+//------------------------------------------------FunÃ§Ãµes----------------------------------------------------------------//
 
 main()
 {
@@ -21,16 +21,16 @@ main()
 	setlocale(LC_ALL, "Portuguese");
 	int var_int1, var_int2;
 	float var_float;
-	printf("Digite um valor (variável int): ");
+	printf("Digite um valor (variÃ¡vel int): ");
 	scanf("%d", &var_int1);
-	printf("Digite um valor (variável int): ");
+	printf("Digite um valor (variÃ¡vel int): ");
 	scanf("%d", &var_int2);
-	printf("Digite um valor (variável float): ");
+	printf("Digite um valor (variÃ¡vel float): ");
 	scanf("%f", &var_float);
 	printf("\nA soma dos dois valores inteiros: %d.", var_int1 + var_int2);
 	printf("\nO dobro do primeiro valor inteiro somado com o dobro do segundo valor inteiro: %d.", (var_int1 * 2) + (var_int2 * 2));
 	printf("\nO valor float multiplicado por 6.5: %.2f.", var_float * 6.5);
-	printf("\nA soma dos três valores: %.2f.\n\n", var_float + var_int1 + var_int2);
+	printf("\nA soma dos trÃªs valores: %.2f.\n\n", var_float + var_int1 + var_int2);
 	system("pause");
 	
 }
