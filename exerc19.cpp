12aa0cae06faec0bd768f29740b7ec28c8e4a039
Gabriel Tavares da Silva <93527457+gabrieltavares184@users.noreply.github.com>
2021-11-03 21:58:07 -0300

    Rename Exercício 19.cpp to exerc19.cpp

--- exerc19.cpp
+++ exerc19.cpp
@@ -1,4 +1,4 @@
-/* Exercício 19: Escreva um programa que leia o total de horas que uma pessoa utiliza o whatsapp durante um dia. Calcule e mostre a porcentagem
+/* ExercÃ­cio 19: Escreva um programa que leia o total de horas que uma pessoa utiliza o whatsapp durante um dia. Calcule e mostre a porcentagem
 de horas neste aplicativo.
 
    1dia ---> 24h ---> 100% */
@@ -9,17 +9,17 @@ de horas neste aplicativo.
 #include <stdlib.h>
 #include <locale.h>
 
-//------------------------------------------------Funções----------------------------------------------------------------//
+//------------------------------------------------FunÃ§Ãµes----------------------------------------------------------------//
 
 main()
 {
 	
 	setlocale(LC_ALL, "Portuguese");
-	printf("Digite o total de horas utilizadas por você no Whatsapp, durante um dia: ");
+	printf("Digite o total de horas utilizadas por vocÃª no Whatsapp, durante um dia: ");
 	float horas;
 	scanf("%f", &horas);
 	float porcentagem = horas / 24 * 100;
-	printf("Você utilizou o Whatsapp em: %.1f %% do seu dia.\n\n", porcentagem); // %%: É um código de formatação que apresenta o caractere %.
+	printf("VocÃª utilizou o Whatsapp em: %.1f %% do seu dia.\n\n", porcentagem); // %%: Ã‰ um cÃ³digo de formataÃ§Ã£o que apresenta o caractere %.
 	system("pause");
 	
 }
