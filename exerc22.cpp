79a6b442ce38edf6a1816faadd90904813ed0dd4
Gabriel Tavares da Silva <93527457+gabrieltavares184@users.noreply.github.com>
2021-11-03 21:58:44 -0300

    Rename Exercício 22.cpp to exerc22.cpp

--- exerc22.cpp
+++ exerc22.cpp
@@ -1,5 +1,5 @@
-/* ExercÌcio 22: Elabore um programa que receba (leia com scanf) o valor do raio de uma esfera (tipo float). Calcule e apresente o volume e a ·rea da
-esfera, baseando-se nas fÛrmulas dadas. Utilize para pi o valor constante de 3.14. Apresente o resultado com 1 casa decimal (%.1f). */
+/* Exerc√≠cio 22: Elabore um programa que receba (leia com scanf) o valor do raio de uma esfera (tipo float). Calcule e apresente o volume e a √°rea da
+esfera, baseando-se nas f√≥rmulas dadas. Utilize para pi o valor constante de 3.14. Apresente o resultado com 1 casa decimal (%.1f). */
 
 //------------------------------------------------Bibliotecas------------------------------------------------------------//
 
@@ -7,7 +7,7 @@ esfera, baseando-se nas fÛrmulas dadas. Utilize para pi o valor constante de 3.1
 #include <stdlib.h>
 #include <locale.h>
 
-//------------------------------------------------FunÁıes----------------------------------------------------------------//
+//------------------------------------------------Fun√ß√µes----------------------------------------------------------------//
 
 main()
 {
@@ -19,7 +19,7 @@ main()
 	float volume =  (4 / 3) * 3.14 * (raio * raio * raio);
 	float area = 4 * 3.14 * (raio * raio);
 	printf("\nVolume: %.1f.", volume);
-	printf("\n¡rea: %.1f.\n\n", area);
+	printf("\n√Årea: %.1f.\n\n", area);
 	system("pause");
 		
 }
