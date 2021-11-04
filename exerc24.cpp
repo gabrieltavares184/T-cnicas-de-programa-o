3db964508ca916e94807e3981c8498d3735dda5e
Gabriel Tavares da Silva <93527457+gabrieltavares184@users.noreply.github.com>
2021-11-03 21:59:24 -0300

    Rename Exercício 24.cpp to exerc24.cpp

--- exerc24.cpp
+++ exerc24.cpp
@@ -1,5 +1,5 @@
-/* ExercÌcio 24: FaÁa um programa que leia o peso de uma pessoa em kg e a sua altura em metros (vari·veis tipo float). Calcular e apresentar o
-resultado do IMC (Õndice de Massa CorpÛrea) da pessoa. Apresente o resultado com 1 casa decimal (%.1f).
+/* Exerc√≠cio 24: Fa√ßa um programa que leia o peso de uma pessoa em kg e a sua altura em metros (vari√°veis tipo float). Calcular e apresentar o
+resultado do IMC (√çndice de Massa Corp√≥rea) da pessoa. Apresente o resultado com 1 casa decimal (%.1f).
 
    IMC = Peso / (Altura * Altura) */
 
@@ -10,14 +10,14 @@ resultado do IMC (Õndice de Massa CorpÛrea) da pessoa. Apresente o resultado com
 #include <locale.h>
 #include <conio.c>
 
-//------------------------------------------------FunÁıes----------------------------------------------------------------//
+//------------------------------------------------Fun√ß√µes----------------------------------------------------------------//
 
 main()
 {
 	
 	setlocale(LC_ALL, "Portuguese");
 	textcolor(10);
-	printf("> IMC (Õndice de Massa CorpÛrea) <");
+	printf("> IMC (√çndice de Massa Corp√≥rea) <");
 	textcolor(15);
 	printf("\n\nDigite a sua massa (em quilogramas): ");
 	float massa;
@@ -30,7 +30,7 @@ main()
 	scanf("%f", &altura);
 	float imc = massa / (altura * altura);
 	textcolor(15);
-	printf("O seu IMC (Õndice de Massa CorpÛrea) È: %.1f.\n\n", imc);
+	printf("O seu IMC (√çndice de Massa Corp√≥rea) √©: %.1f.\n\n", imc);
 	system("pause");
 	
 }
