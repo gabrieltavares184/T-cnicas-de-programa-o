355332ac7e6250962719fa7c031cd62d33d0e7d3
Gabriel Tavares da Silva <93527457+gabrieltavares184@users.noreply.github.com>
2021-11-03 21:58:29 -0300

    Rename Exercício 21.cpp to exerc21.cpp

--- exerc21.cpp
+++ exerc21.cpp
@@ -1,6 +1,6 @@
-/* ExercÌcio 21: Uma empresa resolveu cobrar o uso de games em seu site por horas de acesso. FaÁa um programa que acesse o n˙mero de horas que
+/* Exerc√≠cio 21: Uma empresa resolveu cobrar o uso de games em seu site por horas de acesso. Fa√ßa um programa que acesse o n√∫mero de horas que
 um jogador quer utilizar, calcule e mostre o valor sabendo que a hora de jogo custa R$2,59. Apresente o resultado com 2 casas decimais
-porque È valor monet·rio na moeda local R$ (%.2f). */
+porque √© valor monet√°rio na moeda local R$ (%.2f). */
 
 //------------------------------------------------Bibliotecas------------------------------------------------------------//
 
@@ -8,16 +8,16 @@ porque È valor monet·rio na moeda local R$ (%.2f). */
 #include <stdlib.h>
 #include <locale.h>
 
-//------------------------------------------------FunÁıes----------------------------------------------------------------//
+//------------------------------------------------Fun√ß√µes----------------------------------------------------------------//
 
 main()
 {
 	
 	setlocale(LC_ALL, "Portuguese");
-	printf("Digite o n˙mero de horas que vocÍ deseja utilizar: ");
+	printf("Digite o n√∫mero de horas que voc√™ deseja utilizar: ");
 	float horas;
 	scanf("%f", &horas);
-	float valor = horas * 2.59; // Esta vari·vel È respons·vel pelo armazenamento e c·lculo do valor que o usu·rio dever· pagar.
+	float valor = horas * 2.59; // Esta vari√°vel √© respons√°vel pelo armazenamento e c√°lculo do valor que o usu√°rio dever√° pagar.
 	printf("\nValor total: R$ %.2f.\n\n", valor);
 	system("pause");
 	
